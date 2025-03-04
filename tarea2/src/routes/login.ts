81b9732b5f59f1c0b586c1d778adf7396f14e3af
XrpMinings <admin@xrpminings.com>
2025-03-04 12:08:07 -0600

    Tarea 2 terminada

--- tarea2/src/routes/login.ts
+++ tarea2/src/routes/login.ts
@@ -5,7 +5,6 @@ const router = Router();
 const SECRET_KEY = process.env.JWT_SECRET || 'soyunsecreto';
 
 router.post('/', (req: Request, res: Response) => {
-    //console.log(req.body);
     const { correo, password } = req.body;
     if (!correo || !password) {
         res.status(400).json({ error: 'Correo y password son requeridos' });
